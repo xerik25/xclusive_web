9fa43d6a9fe08cc316f46759d2c9b2282bd6f004
Landon Cline <landonc@hightouchinc.com>
2014-08-07 08:27:23 -0500

    updated footer...added some social media icons

--- footer.php
+++ footer.php
@@ -1,4 +1,30 @@
-    <footer>
+    <footer id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 animated hiding" data-animation="fadeInUp">
+            <ul class="social list-inline ">
+              <li>
+                <a class="facebook" href="https://www.facebook.com/XclusiveEvents?fref=ts" title="Facebook">
+                  <img src="/assets/images/icons/facebook.png">
+                </a>
+              </li>
+              <li>
+                <a class="twitter" href="https://twitter.com/Xevents" title="Twitter">
+                  <img src="/assets/images/icons/twitter.png">
+                </a>
+              </li>
+              <li>
+                <a class="google-plus" href="https://plus.google.com/u/0/113993436534431573809/posts" title="Google+">
+                  <img src="/assets/images/icons/google.png">
+                </a>
+              </li>
+            </ul>
+            <p class="copyright"> 
+              Copyright - Xclusive Bartending</p> 
+            </p>
+          </div>
+        </div>
+      </div>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
       <script src="assets/bower/bootstrap/dist/js/bootstrap.min.js"></script>
       <script src="assets/bower/jquery/dist/jquery.min.js"></script>

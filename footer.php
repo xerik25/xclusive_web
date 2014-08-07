d442e7dcc577c2e57113b2f74c7f985745144d8d
Landon Cline <landonc@hightouchinc.com>
2014-08-07 07:42:49 -0500

    added header and footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="assets/bower/bootstrap/dist/js/bootstrap.min.js"></script>
+      <script src="assets/bower/jquery/dist/jquery.min.js"></script>
+    </footer>
+  </body>
+</html>
